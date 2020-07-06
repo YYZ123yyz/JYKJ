34525417221abc470c1019ec965bebbf82baf05c
yanglongcang <123456>
2020-07-06 13:02:37 +0800

    医生端【聊天室修改】

--- easeui/src/main/java/com/hyphenate/easeui/hyhd/model/Constant.java
+++ easeui/src/main/java/com/hyphenate/easeui/hyhd/model/Constant.java
@@ -56,7 +56,5 @@ public class Constant extends EaseConstant{
 	 */
 	public final static String SERVICEURL = "http://114.215.137.171:28080/";
 
-   public static String doctorUrl;
-	public static String patientUrl;
 
 }
