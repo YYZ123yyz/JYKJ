61642662998368b3a4243e9dbcd1f80c1fa5b836
qiuxinhai <qiuxinhai@126.com>
2020-07-23 11:25:52 +0800

    修改了项目配置

--- app/src/main/java/www/jykj/com/jykj_zxyl/service/MessageReciveService.java
+++ app/src/main/java/www/jykj/com/jykj_zxyl/service/MessageReciveService.java
@@ -26,6 +26,7 @@ import com.hyphenate.chat.EMMessage;
 import com.hyphenate.easeui.hyhd.model.CallReceiver;
 import com.hyphenate.easeui.model.EaseAtMessageHelper;
 import com.hyphenate.easeui.model.EaseDingMessageHelper;
+import com.hyphenate.easeui.utils.EaseCommonUtils;
 
 import java.util.List;
 import java.util.Timer;
@@ -206,11 +207,13 @@ public class MessageReciveService extends Service {
 //            if (progress == 1) {
 //                mBuilder.setDefaults(Notification.DEFAULT_SOUND);
 //            }
-            String str = "{"+messages.get(0).getBody().toString()+"}";
-            EMMessageEntity emMessageEntity = new Gson().fromJson(str,EMMessageEntity.class);
+//            String str = "{"+messages.get(0).getBody().toString()+"}";
+//            EMMessageEntity emMessageEntity = new Gson().fromJson(str,EMMessageEntity.class);
+
+            String ticker = EaseCommonUtils.getMessageDigest(messages.get(0), this);
             remoteViews.setImageViewResource(R.id.image, R.mipmap.logo);
             remoteViews.setTextViewText(R.id.title, "您有一条新消息");
-            remoteViews.setTextViewText(R.id.content, emMessageEntity.getTxt());
+            remoteViews.setTextViewText(R.id.content,ticker);
 //            remoteViews.setProgressBar(R.id.pBar, 10, progress, false);
 //            remoteViews.setTextViewText(R.id.proNum, progress + "/10");
             notificationManager.notify(20, mBuilder.build());
