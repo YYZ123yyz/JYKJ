5670886c9b973ca373d8ff48e58da154dfc17472
gaoguoyuan <gaoguoyuan18@163.com>
2020-11-18 10:24:01 +0800

    语音录入

--- app_base/src/main/java/www/jykj/com/jykj_zxyl/app_base/base_activity/BaseActivity.java
+++ app_base/src/main/java/www/jykj/com/jykj_zxyl/app_base/base_activity/BaseActivity.java
@@ -6,22 +6,43 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 
+import com.blankj.utilcode.util.KeyboardUtils;
+import com.blankj.utilcode.util.SizeUtils;
 import com.gyf.immersionbar.ImmersionBar;
 import com.hyphenate.EMConnectionListener;
 import com.hyphenate.EMError;
 import com.hyphenate.chat.EMClient;
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
 import com.smarx.notchlib.NotchScreenManager;
 
 import butterknife.ButterKnife;
@@ -30,6 +51,8 @@ import io.reactivex.internal.schedulers.ExecutorScheduler;
 import www.jykj.com.jykj_zxyl.app_base.R;
 import www.jykj.com.jykj_zxyl.app_base.base_utils.ActivityStackManager;
 import www.jykj.com.jykj_zxyl.app_base.base_utils.AndroidThreadExecutor;
+import www.jykj.com.jykj_zxyl.app_base.base_utils.JsonParserUtil;
+import www.jykj.com.jykj_zxyl.app_base.base_utils.LogUtils;
 import www.jykj.com.jykj_zxyl.app_base.base_utils.SharedPreferences_DataSave;
 
 /**
@@ -51,12 +74,32 @@ public abstract class BaseActivity extends AppCompatActivity {
     private EMConnectionListener emConnectionListener;
     protected int pageIndex=1;
     protected int pageSize=10;
+    private SpeechRecognizer mIat;
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            LogUtils.e("SpeechRecognizer init() code = " + code);
+            if (code != ErrorCode.SUCCESS) {
+                LogUtils.e("初始化失败，错误码：" + code);
+            }
+        }
+    };
+    private PopupWindow popupWindow;
+    private TextView languageTv;
+    private PopupWindow mPopWindow;
+    private View decorView;
+    private int popHigh;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         onBeforeSetContentLayout();
         context = this;
+        String localClassName = getLocalClassName();
+        decorView = this.getWindow().getDecorView();
+        com.blankj.utilcode.util.LogUtils.e("activity    " + localClassName);
         Resources res = getResources();
         if (res.getConfiguration().fontScale != 1) {//非默认值
             Configuration newConfig = new Configuration();
@@ -75,6 +118,7 @@ public abstract class BaseActivity extends AppCompatActivity {
             initImmersionBar();
             setStatusBarColor();
         }
+        initSpeech();
         //初始化view
         initView();
         //初始化数据
@@ -83,6 +127,27 @@ public abstract class BaseActivity extends AppCompatActivity {
         setListener();
         //添加Im监听
         addImConnectionListener();
+
+
+        KeyboardUtils.registerSoftInputChangedListener(this, new KeyboardUtils.OnSoftInputChangedListener() {
+            @Override
+            public void onSoftInputChanged(int height) {
+                if (height > 0) {
+                    if (!localClassName.contains("ChatActivity")) {
+
+                        showPopupCommnet();
+                    }
+                } else {
+                    if (popupWindow != null) {
+                        popupWindow.dismiss();
+                    }
+                    if (mPopWindow != null){
+                        mPopWindow.dismiss();
+                    }
+
+                }
+            }
+        });
     }
 
 
@@ -280,5 +345,207 @@ public abstract class BaseActivity extends AppCompatActivity {
         unbinder.unbind();
     }
 
+    private void initSpeech() {
+        mIat = SpeechRecognizer.createRecognizer(this, mInitListener);
+        //设置语法ID和 SUBJECT 为空，以免因之前有语法调用而设置了此参数；或直接清空所有参数，具体可参考 DEMO 的示例。
+        mIat.setParameter(SpeechConstant.CLOUD_GRAMMAR, null);
+        mIat.setParameter(SpeechConstant.SUBJECT, null);
+        //设置返回结果格式，目前支持json,xml以及plain 三种格式，其中plain为纯听写文本内容
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+        //此处engineType为“cloud”
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, "cloud");
+        //设置语音输入语言，zh_cn为简体中文
+        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        //设置结果返回语言
+        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+        // 设置语音前端点:静音超时时间，单位ms，即用户多长时间不说话则当做超时处理
+        //取值范围{1000～10000}
+        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
+        //设置语音后端点:后端点静音检测时间，单位ms，即用户停止说话多长时间内即认为不再输入，
+        //自动停止录音，范围{0~10000}
+        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
+        //设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT, "1");
+    }
 
+    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+        @Override
+        public void onBeginOfSpeech() {
+
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+
+        }
+
+        @Override
+        public void onResult(RecognizerResult results, boolean isLast) {
+
+
+            LogUtils.e(results.getResultString());
+
+            /* printResult(results);*/
+
+            String text = JsonParserUtil.parseIatResult(results.getResultString());
+
+
+            View view = getWindow().getDecorView().findFocus();
+
+
+            if (view instanceof EditText) {
+                ((EditText) view).append(text);
+            }
+
+        }
+
+        @Override
+        public void onVolumeChanged(int volume, byte[] data) {
+
+
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+
+        }
+    };
+
+
+    private void showPopupCommnet() {
+        if (popupWindow !=null ){
+            if (popupWindow.isShowing()){
+                return;
+            }
+        }
+        View view = LayoutInflater.from(this).inflate(
+                R.layout.voice_popupwindow, null);
+        LinearLayout part1 = view.findViewById(R.id.layout_part_1);
+        languageTv = view.findViewById(R.id.choose_lang);
+        final View decorView = this.getWindow().getDecorView();
+        final Rect outRect = new Rect();
+        decorView.getWindowVisibleDisplayFrame(outRect);
+
+
+        popHigh = decorView.getBottom() - outRect.bottom;
+        view.findViewById(R.id.iv_input_voice).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIat.isListening()) {
+                    mIat.stopListening();
+                }
+            }
+        });
+
+
+        view.findViewById(R.id.iv_input_voice).setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                mIat.startListening(mRecognizerListener);
+                return false;
+            }
+        });
+        view.findViewById(R.id.lanuage_choose).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                optionPick.show(decorView);
+//                initChoose ();
+                showPopupWindow();
+
+            }
+        });
+
+//        initChangeLanguage();
+
+        popupWindow = new PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT, true);
+
+        popupWindow.setTouchable(true);
+        popupWindow.setTouchInterceptor(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+        popupWindow.setFocusable(false);
+        popupWindow.setOutsideTouchable(false);
+        popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, (decorView.getBottom() - outRect.bottom));
+        ColorDrawable cd = new ColorDrawable(0x000000);
+        popupWindow.setBackgroundDrawable(cd);
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        popupWindow.update();
+
+
+    }
+
+
+    private void showPopupWindow(){
+
+        if (mPopWindow !=null ){
+            if (mPopWindow.isShowing()){
+                return;
+            }
+        }
+
+
+        View view = LayoutInflater.from(this).inflate(R.layout.voice_popupwindow_1, null);
+
+
+        mPopWindow = new PopupWindow(view, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        mPopWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mPopWindow.setFocusable(false);
+
+  /*      mPopWindow.setInputMethodMode(PopupWindow.INPUT_METHOD_NEEDED);
+        mPopWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE );*/
+        view.findViewById(R.id.lanuage_nomal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+                languageTv.setText("普通话");
+
+                mPopWindow.dismiss();
+            }
+        });
+        view.findViewById(R.id.lanuage_english).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIat.setParameter(SpeechConstant.LANGUAGE, "en_us");
+                languageTv.setText("英语");
+                mPopWindow.dismiss();
+            }
+        });
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+
+        mPopWindow.setOutsideTouchable(false);
+
+        mPopWindow.update();
+        // 设置背景颜色变暗
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha = 0.7f;
+        getWindow().setAttributes(lp);
+        mPopWindow.setWidth(width-70);
+
+        mPopWindow.showAtLocation(decorView,
+                Gravity.BOTTOM, 0, popHigh + SizeUtils.dp2px(90));
+        mPopWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+
+            @Override
+            public void onDismiss() {
+                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                lp.alpha = 1f;
+                getWindow().setAttributes(lp);
+            }
+        });
+    }
 }
