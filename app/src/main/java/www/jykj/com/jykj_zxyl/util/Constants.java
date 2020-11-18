2c5b1b2e485d69567ea5fe9e62c74db5e04dda02
yangyunze <1147286394@qq.com>
2020-11-18 18:39:55 +0800

    全局改变字体大小

---
+++ app/src/main/java/www/jykj/com/jykj_zxyl/util/Constants.java
@@ -0,0 +1,7 @@
+package www.jykj.com.jykj_zxyl.util;
+
+public class Constants {
+
+    public static final String SP_FontScale="字体大小调整";
+    public static final String SP_MultiLanguage="SP_MultiLanguage";
+}
