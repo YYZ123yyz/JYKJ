7e522b7247d288f9b84001f652f19a6aed994a35
qiuxinhai <qiuxinhai@126.com>
2020-12-25 17:46:58 +0800

    修改了零钱明细列表逻辑。

--- app/src/main/java/www/jykj/com/jykj_zxyl/capitalpool/adapter/AccountBalanceInfoListAdapter.java
+++ app/src/main/java/www/jykj/com/jykj_zxyl/capitalpool/adapter/AccountBalanceInfoListAdapter.java
@@ -34,14 +34,25 @@ public class AccountBalanceInfoListAdapter extends BaseQuickAdapter<AccountBalan
         TextView tvBillTitle = helper.getView(R.id.tv_bill_title);
         ImageView ivMoneySign = helper.getView(R.id.iv_money_sign);
         TextView tvDateTime = helper.getView(R.id.tv_date_time);
-        tvBillTitle.setText(String.format("%s %s", item.getInfoDesc(), item.getMainUserName()));
+        TextView tvBillContent = helper.getView(R.id.tv_bill_content);
+        TextView tvAmount = helper.getView(R.id.tv_amount);
         int changeType = item.getChangeType();
+        int changeChildType = item.getChangeChildType();
         if (changeType==1) {
             ivMoneySign.setImageResource(R.mipmap.bg_return_money);
+            if (changeChildType==11) {
+                tvBillTitle.setText(String.format("%s -%s -%s", item.getChangeTypeName(), item.getOrderTypeName(), item.getSourceUserName()));
+            }else{
+                tvBillTitle.setText(item.getChangeTypeName());
+            }
+
         }else if(changeType==2){
             ivMoneySign.setImageResource(R.mipmap.bg_pay_money);
+            tvBillTitle.setText(String.format("%s  %s", item.getOrderTypeName(), item.getSourceUserName()));
         }
-
+        tvBillContent.setText(item.getInfoDesc());
+        String infoMoney = item.getInfoMoney();
+        tvAmount.setText(infoMoney);
         tvDateTime.setText(DateUtils.getDateToStringYYYMMDDHHMM(item.getInfoDateTime()));
 
     }
