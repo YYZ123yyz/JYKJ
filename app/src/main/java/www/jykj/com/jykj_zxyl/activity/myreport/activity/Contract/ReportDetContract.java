e16dc42e8d5cb54c08e9d2a7a1e4f35255aef01b
qiuxinhai <qiuxinhai@126.com>
2021-01-01 16:26:19 +0800

    修改了部分逻辑。

--- app/src/main/java/www/jykj/com/jykj_zxyl/activity/myreport/activity/Contract/ReportDetContract.java
+++ app/src/main/java/www/jykj/com/jykj_zxyl/activity/myreport/activity/Contract/ReportDetContract.java
@@ -2,7 +2,6 @@ package www.jykj.com.jykj_zxyl.activity.myreport.activity.Contract;
 
 import java.util.List;
 
-import www.jykj.com.jykj_zxyl.activity.myreport.activity.ReportBean;
 import www.jykj.com.jykj_zxyl.app_base.mvp.BasePresenter;
 import www.jykj.com.jykj_zxyl.app_base.mvp.BaseView;
 
