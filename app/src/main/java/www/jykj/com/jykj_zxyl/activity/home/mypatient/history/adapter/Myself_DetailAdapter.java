d8857ed7901febb003995fd4f065771b27a9c264
qiuxinhai <qiuxinhai@126.com>
2021-01-11 16:07:32 +0800

    添加了用户认证逻辑。

--- app/src/main/java/www/jykj/com/jykj_zxyl/activity/home/mypatient/history/adapter/Myself_DetailAdapter.java
+++ app/src/main/java/www/jykj/com/jykj_zxyl/activity/home/mypatient/history/adapter/Myself_DetailAdapter.java
@@ -1,5 +1,6 @@
 package www.jykj.com.jykj_zxyl.activity.home.mypatient.history.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,26 +28,33 @@ import yyz_exploit.Utils.MyImageView;
 public class Myself_DetailAdapter extends RecyclerView.Adapter<Myself_DetailAdapter.ViewHolder>  {
     private List<String> datas;
     private Context mContext;
-
+    private int width;
     public Myself_DetailAdapter(List<String> datas, Context mContext) {
         this.datas = datas;
         this.mContext = mContext;
+        Display defaultDisplay = ((Activity) mContext).getWindowManager().getDefaultDisplay();
+        width = defaultDisplay.getWidth();
     }
 
     @NonNull
     @Override
     public Myself_DetailAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_myself_detail,viewGroup,false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_myself_detail,
+                viewGroup,false);
        ViewHolder vh = new ViewHolder(view);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        String s = datas.get(i);
+        String url = datas.get(i);
 //        Uri uri = Uri.parse(s);
-        Glide.with(mContext).load("http://114.215.137.171:8040/patientImage/patientlogo/df90285cf3be47e2b74f24837f0b4652/Patientlogo_20200902183522.jpg").into(viewHolder.item_img);
 
+        ViewGroup.LayoutParams layoutParams = viewHolder.item_img.getLayoutParams();
+        layoutParams.height=width/3;
+        layoutParams.width=width/3;
+        viewHolder.item_img.setLayoutParams(layoutParams);
+        Glide.with(mContext).load(url).into(viewHolder.item_img);
     }
 
     @Override
